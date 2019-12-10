0bea1de1756d01961ddaaba4fb0a6a712cb1fb66
Mathias Quintero <me@quintero.io>
2019-12-10 18:38:47 +0100

    Sorting structs before generating code

--- Graphaello/Commands/Codegen/Codegen.swift
+++ Graphaello/Commands/Codegen/Codegen.swift
@@ -11,6 +11,11 @@ import Foundation
 struct Codegen {
     let apis: [API]
     let structs: [GraphQLStruct]
+    
+    init(apis: [API], structs: [GraphQLStruct]) {
+        self.apis = apis
+        self.structs = structs.sorted { $0.definition.name <= $1.definition.name }
+    }
 }
 
 extension Codegen {
