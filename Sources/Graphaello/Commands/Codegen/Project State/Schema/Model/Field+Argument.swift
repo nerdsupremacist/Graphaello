fdf0f51eb16986666983fed8c3d78a30ef755bf4
Mathias Quintero <me@quintero.io>
2019-12-18 18:34:25 +0100

    Moving property wrapper to extra file

--- Sources/Graphaello/Commands/Codegen/Project State/Schema/Model/Field+Argument.swift
+++ Sources/Graphaello/Commands/Codegen/Project State/Schema/Model/Field+Argument.swift
@@ -21,22 +21,3 @@ extension Schema.GraphQLType.Field {
     }
 
 }
-
-@propertyWrapper
-struct OptionalParsed<Lexer: LexerProtocol, Value: Parsable & Hashable>: Codable, Hashable where Lexer.Token == Value.Token {
-    private let stringRepresentation: String?
-    var wrappedValue: Value?
-
-    private init(stringRepresentation: String?) throws {
-        self.stringRepresentation = stringRepresentation
-        self.wrappedValue = try stringRepresentation.map { try Value.parse($0, using: Lexer.self) }
-    }
-
-    init(from decoder: Decoder) throws {
-        try self.init(stringRepresentation: Optional<String>(from: decoder))
-    }
-
-    func encode(to encoder: Encoder) throws {
-        try stringRepresentation.encode(to: encoder)
-    }
-}
---
+++ Sources/Graphaello/Property Wrappers/OptionalParsed.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mathias Quintero on 12/18/19.
+//
+
+import Foundation
+import Ogma
+
+@propertyWrapper
+struct OptionalParsed<Lexer: LexerProtocol, Value: Parsable & Hashable>: Codable, Hashable where Lexer.Token == Value.Token {
+    private let stringRepresentation: String?
+    var wrappedValue: Value?
+
+    private init(stringRepresentation: String?) throws {
+        self.stringRepresentation = stringRepresentation
+        self.wrappedValue = try stringRepresentation.map { try Value.parse($0, using: Lexer.self) }
+    }
+
+    init(from decoder: Decoder) throws {
+        try self.init(stringRepresentation: Optional<String>(from: decoder))
+    }
+
+    func encode(to encoder: Encoder) throws {
+        try stringRepresentation.encode(to: encoder)
+    }
+}
