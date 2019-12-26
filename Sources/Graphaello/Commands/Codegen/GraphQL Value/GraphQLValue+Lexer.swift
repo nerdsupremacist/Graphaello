6e01bd64ae0cc6c7a7670559d94b64daeffd6d82
Mathias Quintero <me@quintero.io>
2019-12-26 20:48:41 +0100

    Using new string literal syntax for regex

--- Sources/Graphaello/Commands/Codegen/GraphQL Value/GraphQLValue+Lexer.swift
+++ Sources/Graphaello/Commands/Codegen/GraphQL Value/GraphQLValue+Lexer.swift
@@ -14,16 +14,16 @@ extension GraphQLValue {
         typealias Token = GraphQLValue.Token
 
         static let generators: Generators = [
-            RegexTokenGenerator(pattern: "\\{").map(to: .openCurlyBracket),
-            RegexTokenGenerator(pattern: "\\}").map(to: .closeCurlyBracket),
-            RegexTokenGenerator(pattern: "\\[").map(to: .openSquareBracket),
-            RegexTokenGenerator(pattern: "\\]").map(to: .closeSquareBracket),
+            RegexTokenGenerator(pattern: #"\{"#).map(to: .openCurlyBracket),
+            RegexTokenGenerator(pattern: #"\}"#).map(to: .closeCurlyBracket),
+            RegexTokenGenerator(pattern: #"\["#).map(to: .openSquareBracket),
+            RegexTokenGenerator(pattern: #"\]"#).map(to: .closeSquareBracket),
             RegexTokenGenerator(pattern: ",").map(to: .comma),
             RegexTokenGenerator(pattern: ":").map(to: .colon),
-            RegexTokenGenerator(pattern: "true\\b").map(to: .true),
-            RegexTokenGenerator(pattern: "false\\b").map(to: .false),
-            RegexTokenGenerator(pattern: "null\\b").map(to: .null),
-            RegexTokenGenerator(pattern: "[a-zA-Z][a-zA-Z\\-_0-9]*").map(Token.identifier),
+            RegexTokenGenerator(word: "true").map(to: .true),
+            RegexTokenGenerator(word: "false").map(to: .false),
+            RegexTokenGenerator(word: "null").map(to: .null),
+            RegexTokenGenerator(pattern: #"[a-zA-Z][a-zA-Z\-_0-9]*"#).map(Token.identifier),
 
             StringLiteralTokenGenerator().map(Token.string),
             DoubleLiteralTokenGenerator().map(Token.double),
