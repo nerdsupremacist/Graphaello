084f8f414ed6d9213e29ee3380013dde5184734d
Mathias Quintero <me@quintero.io>
2020-01-02 17:15:41 +0100

    Formatting

--- Sources/Graphaello/Processing/Pipeline/Component/5. Cleaning/SubCleaners/FieldName/Implementation/GraphQLObjectFieldNameCleaner.swift
+++ Sources/Graphaello/Processing/Pipeline/Component/5. Cleaning/SubCleaners/FieldName/Implementation/GraphQLObjectFieldNameCleaner.swift
@@ -39,7 +39,6 @@ struct GraphQLObjectFieldNameCleaner: FieldNameCleaner {
                                  fragments: fragments,
                                  typeConditionals: typeConditionals.value,
                                  arguments: resolved.arguments)
-            
         }
     }
     
