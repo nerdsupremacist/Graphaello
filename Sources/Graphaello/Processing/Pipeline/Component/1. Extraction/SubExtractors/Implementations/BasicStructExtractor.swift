ad44c0a2048ca0ebe2de9f2672f64eed50edd081
Mathias Quintero <me@quintero.io>
2020-01-12 04:53:45 +0100

    removing properties with values from the extracted result

--- Sources/Graphaello/Processing/Pipeline/Component/1. Extraction/SubExtractors/Implementations/BasicStructExtractor.swift
+++ Sources/Graphaello/Processing/Pipeline/Component/1. Extraction/SubExtractors/Implementations/BasicStructExtractor.swift
@@ -15,7 +15,7 @@ struct BasicStructExtractor: StructExtractor {
         let properties = try code.substructure()
             .filter { try $0.kind() == .varInstance }
             .compactMap { try propertyExtractor.extract(code: $0) }
-            .filter { try !$0.isComputed() }
+            .filter { try !$0.isComputed() && !$0.hasValue() }
 
         return Struct(code: code,
                       name: try code.name(),
---
+++ Sources/Graphaello/Processing/Pipeline/Stage/1. Extracted/Extracted+Property+hasValue.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mathias Quintero on 12.01.20.
+//
+
+import Foundation
+import SwiftSyntax
+
+extension Property where CurrentStage == Stage.Extracted {
+    
+    func hasValue() throws -> Bool {
+        guard let expression = try code.syntaxTree().singleItem() as? VariableDeclSyntax else { return false }
+        return expression.bindings.contains { $0.initializer != nil }
+    }
+    
+}
