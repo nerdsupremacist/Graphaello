c88fd1ce9427961b2dd06319481f8b34df61912d
Mathias Quintero <me@quintero.io>
2020-01-16 00:57:38 +0100

    Cleanign up operation type calidator pattern matching

--- Sources/Graphaello/Processing/Pipeline/Component/3. Validation/SubValidators/Implementation/BasicOperationTypeValidator.swift
+++ Sources/Graphaello/Processing/Pipeline/Component/3. Validation/SubValidators/Implementation/BasicOperationTypeValidator.swift
@@ -22,12 +22,30 @@ struct BasicOperationTypeValidator: OperationTypeValidator {
         case (.complex(let nonNullDefinition, let ofType), _) where nonNullDefinition.kind == .nonNull:
             return .complex(nonNullDefinition, ofType: try validate(operation: operation, on: ofType))
 
-        case (.complex(let listDefinition, .concrete(let itemDefinition)), .compactMap) where listDefinition.kind == .list:
+        case (
+            .complex(
+                let listDefinition,
+                .concrete(let itemDefinition)
+            ),
+            .compactMap
+        ) where listDefinition.kind == .list:
             return .complex(listDefinition,
                             ofType: .complex(.init(kind: .nonNull, name: nil),
                                              ofType: .concrete(itemDefinition)))
 
-        case (.complex(let outerListDefinition, .complex(let nonNullDefinition, .complex(let innerListDefinition, .concrete(let itemDefinition)))), .flatten) where outerListDefinition.kind == .list && nonNullDefinition.kind == .list && innerListDefinition.kind == .list:
+        case (
+            .complex(
+                let outerListDefinition,
+                .complex(
+                    let nonNullDefinition,
+                    .complex(
+                        let innerListDefinition,
+                        .concrete(let itemDefinition)
+                    )
+                )
+            ),
+            .flatten
+        ) where outerListDefinition.kind == .list && nonNullDefinition.kind == .list && innerListDefinition.kind == .list:
             return .complex(outerListDefinition, ofType: .concrete(itemDefinition))
 
         case (_, .withDefault), (_, .nonNull):
