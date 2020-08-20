44ba473fdb2959029b9f778f1d2f337aa5ecbbaf
Mathias Quintero <me@quintero.io>
2020-08-20 10:57:14 +0200

    Marking opaque types directly as computed

--- Sources/Graphaello/Processing/Pipeline/Stage/1. Extracted/Extracted+Property+isComputed.swift
+++ Sources/Graphaello/Processing/Pipeline/Stage/1. Extracted/Extracted+Property+isComputed.swift
@@ -4,7 +4,10 @@ import SwiftSyntax
 extension Property where CurrentStage == Stage.Extracted {
     
     func isComputed() throws -> Bool {
-        guard !attributes.contains(where: { $0.kind == ._custom }) else { return false }
+        if type.hasPrefix("some ") {
+            return true
+        }
+        
         guard let expression = try code.syntaxTree().singleItem()?.as(VariableDeclSyntax.self) else { return false }
         return expression.bindings.contains { $0.accessor != nil }
     }
