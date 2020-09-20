146f77a5f2722bf722c9a5c96487a3e36fa498d3
Mathias Quintero <me@quintero.io>
2020-09-20 14:39:15 +0200

    fixing finding possible types for interface or union with camelcasing

--- Sources/Graphaello/Extensions/Array.swift
+++ Sources/Graphaello/Extensions/Array.swift
@@ -44,7 +44,8 @@ extension Array where Element == Schema.GraphQLType.Field {
 extension Array where Element == Schema.TypeReference {
 
     subscript(name: String) -> Element? {
-        return first { $0.name == name }
+        let name = name.upperCamelized
+        return first { $0.name.upperCamelized == name }
     }
 
 }
