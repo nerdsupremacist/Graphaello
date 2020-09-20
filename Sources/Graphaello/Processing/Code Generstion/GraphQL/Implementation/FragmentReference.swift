b212f23c937be1e0eeb1cf8fcf64929d08170f05
Mathias Quintero <me@quintero.io>
2020-09-20 03:07:19 +0200

    fixing flattented fragment references

--- Sources/Graphaello/Processing/Code Generstion/GraphQL/Implementation/FragmentReference.swift
+++ Sources/Graphaello/Processing/Code Generstion/GraphQL/Implementation/FragmentReference.swift
@@ -8,10 +8,24 @@ struct FragmentReference: GraphQLCodeTransformable {
 extension GraphQLObject {
     
     var referencedFragments: [FragmentReference] {
-        return fragments
+        return flattenArgumentFragments
             .sorted { $0.name < $1.name }
             .map { FragmentReference(hasArguments: !$0.arguments.isEmpty,
                                      fragment: $0) }
     }
     
+}
+
+extension GraphQLObject {
+
+    fileprivate var flattenArgumentFragments: [GraphQLFragment] {
+        return fragments.flatMap { fragment -> [GraphQLFragment] in
+            if fragment.arguments.isEmpty {
+                return [fragment]
+            } else {
+                return [fragment] + fragment.object.flattenArgumentFragments
+            }
+        }
+    }
+
 }
