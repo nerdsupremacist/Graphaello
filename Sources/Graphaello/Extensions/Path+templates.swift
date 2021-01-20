b630625d66ae46e2b9706d7b2b2a52a45b33ded5
cgi <cgi@appsfactory.de>
2021-01-20 18:58:00 +0100

    Improve creation and usage of template folder

--- Sources/Graphaello/Extensions/Path+templates.swift
+++ Sources/Graphaello/Extensions/Path+templates.swift
@@ -23,28 +23,16 @@ private func templatePath(file: StaticString = #file) -> Path {
 	
 	let templatesPath = Path("templates")
 	
-    #if DEBUG
-	var path = Path(file.description)
+	var path: Path = {
+		#if DEBUG
+		return Path(file.description)
+		#else
+		return binaryPath
+		#endif
+	}()
+	
 	while (path + templatesPath).exists == false {
 		path = path.parent()
 	}
-    return path + templatesPath
-    #else
-	let graphaelloPath = Path("graphaello") + templatesPath
-	var path = binaryPath
-	var pathExists = false
-	
-	while pathExists == false {
-		if (path + graphaelloPath).exists {
-			path = path + graphaelloPath
-			pathExists = true
-		} else if (path + templatesPath).exists {
-			path = path + templatesPath
-			pathExists = true
-		} else {
-			path = path.parent()
-		}
-	}
-    return path
-    #endif
+	return path + templatesPath
 }
