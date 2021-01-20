9a348923bc95f94eb3c4a63307792f96080fd1cf
cgi <cgi@appsfactory.de>
2021-01-20 15:13:43 +0100

    Support SPM resources

--- Sources/Graphaello/Extensions/Path+templates.swift
+++ Sources/Graphaello/Extensions/Path+templates.swift
@@ -20,9 +20,31 @@ extension Path {
 }
 
 private func templatePath(file: StaticString = #file) -> Path {
+	
+	let templatesPath = Path("templates")
+	
     #if DEBUG
-    return Path(file.description).parent().parent().parent().parent() + "templates"
+	var path = Path(file.description)
+	while (path + templatesPath).exists == false {
+		path = path.parent()
+	}
+    return path + templatesPath
     #else
-    return binaryPath.parent().parent() + "graphaello" + "templates"
+	let graphaelloPath = Path("graphaello") + templatesPath
+	var path = binaryPath
+	var pathExists = false
+	
+	while pathExists == false {
+		if (path + graphaelloPath).exists {
+			path = path + graphaelloPath
+			pathExists = true
+		} else if (path + templatesPath).exists {
+			path = path + templatesPath
+			pathExists = true
+		} else {
+			path = path.parent()
+		}
+	}
+    return path
     #endif
 }
